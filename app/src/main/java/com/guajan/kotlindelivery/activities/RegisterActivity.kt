109cfa9351a7368e7724dc9d493932130a94c07f
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-19 11:04:51 -0500

    12. Diseño de la pantalla de registro

--- app/src/main/java/com/guajan/kotlindelivery/activities/RegisterActivity.kt
+++ app/src/main/java/com/guajan/kotlindelivery/activities/RegisterActivity.kt
@@ -1,12 +1,26 @@
 package com.guajan.kotlindelivery.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.guajan.kotlindelivery.R
 
 class RegisterActivity : AppCompatActivity() {
+    var imageViewGotoLogin: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        imageViewGotoLogin = findViewById(R.id.imageview_go_to_login)
+
+        imageViewGotoLogin?.setOnClickListener{ goToLogin()}
     }
+
+    private fun goToLogin(){
+        val i = Intent(this,MainActivity::class.java)
+        startActivity(i)
+    }
+
 }
